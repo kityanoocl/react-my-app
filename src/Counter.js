b662e013f9c046466978ab260b595e26566b94b3
KIT YAN (DMA-ISD-OOCLL/HKG) <YANKI2@OOCL.COM>
2020-04-21 15:54:04 +0800

    [Kit] added onIncrease

--- src/Counter.js
+++ src/Counter.js
@@ -4,15 +4,23 @@ export default class Counter extends Component {
     constructor(props) {
         super(props)
     
+        this.onIncrease = this.onIncrease.bind(this)
+
         this.state = {
              number : 0
         }
     }
     
+    onIncrease() {
+        this.setState({
+            number : this.state.number + 1
+        })
+    }
+
     render() {
         return (
             <div>
-                <button>+</button>
+                <button onClick = {this.onIncrease}>+</button>
                 {this.state.number}
                 <button>-</button>
             </div>
